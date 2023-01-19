badfbb7651d77867504aefe7976fb872c16beaf9
Nhan Pham <nhan.pham2@tiki.vn>
2023-01-19 08:37:16 +0700

    Add keymap in terminal mode

--- lua/t4tsster/core/keymaps.lua
+++ lua/t4tsster/core/keymaps.lua
@@ -19,6 +19,9 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+-- Terminal management
+keymap.set("t", "<ESC>", "<C-\\><C-n>") -- Exist terminal mode
+
 --------------------
 -- Plugin keymaps
 --------------------
