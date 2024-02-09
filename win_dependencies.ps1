e5cb1af5a4745f2e833043e6f935a96df4ce77ad
Nathan Pham <tendaigom@gmail.com>
2024-02-09 11:58:55 +0700

    Update win_dependencies.ps1

--- win_dependencies.ps1
+++ win_dependencies.ps1
@@ -1,4 +1,15 @@
+# Upgrade to PowerShell 7
+# Check PowerShell version and upgrade if necessary.
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+    Write-Host "Upgrading to PowerShell 7..."
+    # Install PowerShell 7
+    Invoke-Expression "& { iwr https://aka.ms/install-powershell.ps1 -UseBasicParsing | iex }"
+    Write-Host "PowerShell 7 installed. Please restart the session to use PowerShell 7."
+    return
+}
+
 # Install Chocolatey
+Write-Host "Installing Chocolatey..."
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Wait for Chocolatey to be available in the current session
@@ -7,13 +18,12 @@ while (!(Get-Command choco -ErrorAction SilentlyContinue)) {
     Start-Sleep -Seconds 2
 }
 
-# Install make, fzf, and ripgrep using Chocolatey
+# Install Neovim, make, fzf, ripgrep, and Golang using Chocolatey
+Write-Host "Installing Neovim, make, fzf, ripgrep, and Golang..."
+choco install neovim -y
 choco install make -y
 choco install fzf -y
 choco install ripgrep -y
-choco install nerd-fonts-hack -y
-
-# Install Golang
 choco install golang -y
 
 Write-Host "Installation complete."
